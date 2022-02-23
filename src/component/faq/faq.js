78381b72331dca4646807d1a4a2a985283d8c3ef
FxIvan <almendraivan210814@gmail.com>
2022-02-23 20:18:55 -0300

    Updates

--- src/component/faq/faq.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Faq = () =>{
-    return(
-        <div className='text-center mt-5'>
-            <h2>Seccion en Proceso.....</h2>
-        </div>
-    )
-}
